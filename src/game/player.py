76fdb7cc97bb047c5e90e974f9a26fdb7dcbce1a
Lunarmagpie <bambolambo0@gmail.com>
2022-11-03 00:24:26 -0400

    add default deck

--- src/game/player.py
+++ src/game/player.py
@@ -1,11 +1,18 @@
 import dataclasses
 import hikari
 import asyncio
+import functools
+import copy
 
 
 @dataclasses.dataclass
 class Player:
     user: hikari.User
+
+    # Changes during gameplay
+    cards: list[int] = dataclasses.field(
+        default_factory=functools.partial(copy.copy, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])  # type: ignore
+    )
     selected_card: int | None = None
     selected_card_event: asyncio.Event = dataclasses.field(default_factory=asyncio.Event)
 
