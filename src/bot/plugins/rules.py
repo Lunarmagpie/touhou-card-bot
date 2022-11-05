134d31742c911d6c81854cad0cb05b2c5440d4da
Lunarmagpie <bambolambo0@gmail.com>
2022-11-05 01:43:01 -0400

    fix stufff

--- src/bot/plugins/rules.py
+++ src/bot/plugins/rules.py
@@ -5,10 +5,12 @@ from cards.card import Elements, icons
 
 plugin = utils.Plugin()
 
+
 @plugin.include
 @crescent.command(description="View the rules of the game.")
 async def rules(ctx: utils.Context) -> None:
-    await ctx.respond(f"""
+    await ctx.respond(
+        f"""
 Each card belongs to one of 5 different phases: {icons[Elements.FIRE]} Fire, {icons[Elements.WATER]} Water, {icons[Elements.WOOD]} Wood, {icons[Elements.METAL]} Metal, and {icons[Elements.EARTH]} Earth
 
 In addition, each phase obeys 2 different sets of interactions:
@@ -27,4 +29,6 @@ Or, arranged differently:
 To win a round, a card's phase must conquer the other. If two cards are of the same phase, the card with the higher number wins. After a round is won, a seal of the card's phase is obtained.
 
 To win a game, one must collect 1 seal of 4 different phases, or 3 seals of the same phase.
-""", ephemeral=True)
+""",
+        ephemeral=True,
+    )
