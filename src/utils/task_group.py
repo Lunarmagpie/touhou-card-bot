f563980dbea76b7a80cb009c9ebb7da7a57344d0
Lunarmagpie <bambolambo0@gmail.com>
2022-11-06 02:27:17 -0500

    fix mypy

--- src/utils/task_group.py
+++ src/utils/task_group.py
@@ -6,17 +6,17 @@ T = t.TypeVar("T")
 Self = t.TypeVar("Self", bound="TaskGroup")
 
 
-class TaskGroup(contextlib.AbstractAsyncContextManager):
+class TaskGroup(contextlib.AbstractAsyncContextManager[t.Any]):
     def __init__(self) -> None:
-        self.tasks = []
+        self.tasks: list[asyncio.Task[t.Any]] = []
 
     async def __aenter__(self: Self) -> Self:
         return self
 
-    async def __aexit__(self, *_) -> None:
+    async def __aexit__(self, *_: t.Any) -> None:
         await asyncio.gather(*self.tasks)
 
     def create_task(self, awaitable: t.Awaitable[T]) -> asyncio.Task[T]:
-        task = asyncio.create_task(awaitable)
+        task: asyncio.Task[T] = asyncio.create_task(awaitable)  # type: ignore
         self.tasks.append(task)
         return task
